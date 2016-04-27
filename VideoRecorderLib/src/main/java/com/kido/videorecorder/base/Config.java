4814c3d84a6b3ee4d5d7c04757c8e646d6828055
xuguobiao <xuguobiao@myzaker.com>
2016-04-27 11:01:52 +0800

    update config

--- VideoRecorderLib/src/main/java/com/kido/videorecorder/base/Config.java
+++ VideoRecorderLib/src/main/java/com/kido/videorecorder/base/Config.java
@@ -7,14 +7,13 @@ package com.kido.videorecorder.base;
  */
 public class Config {
 
-  public static final int VIDEO_WIDTH = 352;
-  public static final int VIDEO_HEIGHT = 288;
+  public static final int VIDEO_WIDTH = 480;
+  public static final int VIDEO_HEIGHT = 480;
 
   public static final int VIDEO_FRAME_RATE = 16;
   public static final int VIDEO_ENCODING_BIT_RATE = 2 * 1024 * 512;
 
   public static final int VIDEO_ORIENTATION = 90; // vertical: 90
   public static final int VIDEO_MAX_DURATION_SECOND = 15; // second
-
 
 }
